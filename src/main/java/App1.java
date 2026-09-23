1c96d8a23459cb6da2cd06084f0e62e48c8b88f5
ANarendra777 <amarnarenz777@gmail.com>
2026-09-23 18:49:47 +0530

    C2-->Added for loop

--- src/main/java/App1.java
+++ src/main/java/App1.java
@@ -2,5 +2,8 @@ public class App1 {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Java Application!");
         System.out.println("Application deployed successfully.");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
     }
 }
